32af63eef4a6be2dbc9ef4d3fd1101ff1cf39228
Tobiasz <toneq@tlen.pl>
2026-01-30 14:55:52 +0100

    add(site index): dodanie formularza wraz z wyświetlaniem książek

--- views/site/index.php
+++ views/site/index.php
@@ -1,53 +1,82 @@
-<?php
+<!DOCTYPE html>
+<html ng-app="bookApp">
+    <head>
+        <title>e-Instytucja zadanie rekrutacyjne - Tobiasz Tonn</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.8.3/angular.min.js"></script>
+    </head>
 
-/** @var yii\web\View $this */
-
-$this->title = 'My Yii Application';
-?>
-<div class="site-index">
-
-    <div class="jumbotron text-center bg-transparent mt-5 mb-5">
-        <h1 class="display-4">Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+    <body ng-controller="BookController" class="container mt-5">
+        <h2>Dodaj książkę</h2>
+        <form ng-submit="addBook()" class="row g-3">
+            <div class="col-md-6">
+                <input class="form-control" ng-model="book.title" placeholder="Tytuł" required>
             </div>
-            <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+            <div class="col-md-6">
+                <input class="form-control" ng-model="book.author" placeholder="Autor" required>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+            <div class="col-md-4">
+                <input class="form-control" ng-model="book.country" placeholder="Kraj" required>
             </div>
-        </div>
+            <div class="col-md-4">
+                <input class="form-control" ng-model="book.language" placeholder="Język" required>
+            </div>
+            <div class="col-md-4">
+                <input type="number" class="form-control" ng-model="book.pages" placeholder="Liczba stron" required>
+            </div>
+            <div class="col-md-6">
+                <input type="number" class="form-control" ng-model="book.year" placeholder="Rok" required>
+            </div>
+            <div class="col-md-6">
+                <input class="form-control" ng-model="book.imageLink" placeholder="Obrazek dla książki">
+            </div>
+            <div class="col-md-6">
+                <input class="form-control" ng-model="book.link" placeholder="Link do książki">
+            </div>
+            <div class="col-md-6">
+                <button class="btn btn-primary w-100">Dodaj</button>
+            </div>
+        </form>
 
-    </div>
-</div>
+        <hr>
+
+        <h2>Lista książek</h2>
+        <table class="table table-striped">
+            <tr>
+                <th>Obrazek książki</th>
+                <th>Tytuł</th>
+                <th>Autor</th>
+                <th>Rok</th>
+                <th>Liczba stron</th>
+                <th>Link do książki</th>
+            </tr>
+            <tr ng-repeat="b in books">
+                <td><img src="{{b.title}}" alt="{{b.title}}"></td>
+                <td>{{b.title}}</td>
+                <td>{{b.author}}</td>
+                <td>{{b.year}}</td>
+                <td>{{b.pages}}</td>
+                <td><a href="{{b.link}}" target="_blank">{{b.link}}</a></td>
+            </tr>
+        </table>
+
+        <script>
+            angular.module('bookApp', []).controller('BookController', function($scope, $http) {
+                $scope.loadBooks = function() {
+                    $http.get('/api/books').then(function(response) {
+                        $scope.books = response.data;
+                    });
+                };
+
+                $scope.addBook = function() {
+                    $http.post('/api/books', $scope.book).then(function() {
+                        $scope.book = {};
+                        $scope.loadBooks();
+                    });
+                };
+
+                $scope.loadBooks();
+            });
+        </script>
+    </body>
+</html>
